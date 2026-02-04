6d1a9afa891ca717e9435375f04a077f60e4e647
RAHULK2026 <kalyanirahulsable@gmail.com>
2026-02-05 06:37:46 +0900

    import hero and dservices in page .tsx file

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Hero from "./components/feature/homepage/hero";
+import Services from "./components/service";
+
+export default function Page() {
+  return (
+    <>
+      <Hero />
+      <Services />
+    </>
+  );
+}
+
